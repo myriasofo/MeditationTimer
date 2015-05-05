fc305eaa1cbcd8bd8d82472ee94ea99b8bd9dcd1
Abraham Bae <myriasofo@gmail.com>
2015-05-05 13:27:26 -0700

    Working on version that focuses on time remaining
    *Wrapped up version of timer using "time elapsed"
    *Trying version using "time remaining" (as exercise)

--- meditationTimer.py
+++ meditationTimer.py
@@ -16,6 +16,8 @@ class meditationTimer:
         #self.interval = 60.0 * interval_inMinutes
         self.total = 6.0
         self.interval = 2.0
+        self.startTime = time.time()
+        self.prepSounds()
 
     def prepSounds(self):
         """ Uses pygame to load sound files """
@@ -24,35 +26,77 @@ class meditationTimer:
         self.bellEnd = pygame.mixer.Sound(self.dir_sounds + "/shipBell.wav")
         self.bellInterval = pygame.mixer.Sound(self.dir_sounds + "/metalGong.wav")
 
-    def runTimer(self):
-        """ Use sleep() to wait 1sec at a time """
-        self.prepSounds()
-        startTime =time.time()
+    def printEndingStats(self):
+        diff = time.time() - self.startTime
+        print ""
+        print "clock diff: ", diff
+        print "error: ", diff-self.total
 
+    def runTimer_int_elapsed(self):
+        """ 
+        Use sleep() to wait 1sec at a time
+        All vars are in integer seconds
+        """
+        self.total = int(self.total)
+        self.interval = int(self.interval)
+
+        print "elapsed: 0"
         elapsed = 0
         while (elapsed<self.total):
+
             if elapsed%self.interval == 0:
                 self.bellInterval.play()
                 print "bellInterval"
 
             time.sleep(1)
             elapsed += 1
-            print "elapsed: ",elapsed
+            print "elapsed: ", elapsed
 
         # End
         self.bellEnd.play()
-        print ""
-        print "total elapsed: ", elapsed
-        print "end-start: ", time.time() - startTime
-        print "total diff: ", (time.time() - startTime) - elapsed
+        print "bellEnd"
+        self.printEndingStats()
 
+    def runTimer_int_remain(self):
+        """ 
+        Use sleep() to wait 1sec at a time
+        All vars are in integer seconds
+        """
+        self.interval = int(self.interval)
+        remain = int(self.total)
+        print "remain: ", remain
+
+        while (remain>0):
+            if remain%self.interval == 0:
+                self.bellInterval.play()
+                print "bellInterval"
+
+            time.sleep(1)
+            remain -= 1
+            print "remain: ", remain
+
+        # End
+        self.bellEnd.play()
+        print "bellEnd"
+        self.printEndingStats()
 
 def main():
     dir_sounds = 'C:/Users/Abe/Dropbox/CS/apps/meditationTimer/sounds'
     total_inMins = 10
     interval_inMins = 1
     t =meditationTimer(dir_sounds, total_inMins, interval_inMins)
-    t.runTimer()
+    t.runTimer_int_elapsed()
+    #t.runTimer()
+    #t.runTimer_int_remain()
 
 main()
+
+# TODO:
+# wrap up int_remain (make sure it works)
+# work on float_refined (harder than it looks!)
+# What I want
+    # print 0...total (include 0)
+    # ring interval bell at 0, ring bell after each interval secs
+    # ring end bell after total
+    # TRY to get time to be more accurate
 
